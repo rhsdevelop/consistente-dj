ab78f88f0598043bf9f895502b5dc0729dd9a03c
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2025-04-12 15:25:28 -0300

    Refatorar serializers, views e URLs para implementar a estrutura inicial da API financeira com suporte a autenticação JWT e ajustes nos modelos

--- financial/api/urls.py
+++ financial/api/urls.py
@@ -10,10 +10,7 @@ from rest_framework_simplejwt.views import (
 app_name = 'api'
 
 routers = DefaultRouter()
-<<<<<<< HEAD
 routers.register(r'v1/user', views.CustonUser, basename='custonUser')
-=======
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
 routers.register(r'v1/cliente', views.ClienteAPIv1)
 routers.register(r'v1/clienteuser', views.ClienteUserAPIv1)
 routers.register(r'v1/banco', views.BancoAPIv1)
@@ -22,26 +19,14 @@ routers.register(r'v1/parceiro', views.ParceiroAPIv1)
 routers.register(r'v1/contasreceber', views.ContasAReceberAPIv1)
 routers.register(r'v1/contaspagar', views.ContasAPagarAPIv1, basename='contasApagar')
 routers.register(r'v1/transferir', views.TransfirirAPIv1, basename='transferir')
-<<<<<<< HEAD
 routers.register(r'v1/fluxo', views.FluxoCaixaAPIv1, basename='fluxo')
 routers.register(r'v1/resumodiario', views.ResumoDiarioAPIv1, basename='resumoDiario')
 routers.register(r'v1/resumocategoria', views.ResumoCategoriaAPIv1, basename='resumoCategoria')
 routers.register(r'v1/resumoparceiro', views.ResumoParceiroAPIv1, basename='resumoParceiro')
 routers.register(r'v1/cartoes', views.CartoesAPIv1, basename='cartoes')
 
-
 urlpatterns = [
     path('v1/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-=======
-routers.register(r'v1/fluxo', views.FluxoCaixaViewSet, basename='fluxo')
-routers.register(r'v1/resumodiario', views.ResumoDiarioViewSet, basename='resumoDiario')
-routers.register(r'v1/resumocategoria', views.ResumoCategoriaViewSet, basename='resumoCategoria')
-routers.register(r'v1/resumoparceiro', views.ResumoParceiroViewSet, basename='resumoParceiro')
-
-
-urlpatterns = [
-    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
     path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(routers.urls))
--- financial/api/views.py
+++ financial/api/views.py
@@ -1,19 +1,13 @@
 from django.db.models import Q, Sum
 from django.db.models.functions import TruncMonth, TruncDate, TruncDay
 from django.shortcuts import get_object_or_404
-<<<<<<< HEAD
 from django.contrib.auth import get_user_model
-=======
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
-<<<<<<< HEAD
 from rest_framework_simplejwt.views import TokenObtainPairView
-=======
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
 from . import serialazers
 from .helpers.pagination import DefaultPagination
 from manager import models
@@ -21,7 +15,6 @@ from calendar import monthrange
 from datetime import date, datetime, timedelta
 from decimal import Decimal
 
-<<<<<<< HEAD
 class CustonUser(ModelViewSet):
     serializer_class = serialazers.UserSerialazers
 
@@ -30,8 +23,6 @@ class CustonUser(ModelViewSet):
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = serialazers.CustomTokenObtainPairSerializer
 
-=======
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
 class ClienteAPIv1(ModelViewSet):
     queryset = models.ConsistenteCliente.objects.all()
     serializer_class = serialazers.ConsistenteClienteSerialazers
@@ -80,11 +71,7 @@ class ClienteUserAPIv1(ModelViewSet):
                 for valor in valores:
                     q_obj |= Q(**{f"{key}__icontains": valor})
                 filter_seach &= q_obj
-<<<<<<< HEAD
-        parceiros = models.ConsistenteUsuario.objects.filter(filter_seach).order_by('user')
-=======
         parceiros = models.ConsistenteUsuario.objects.filter(filter_seach).order_by('nome')
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
         serialazer = self.get_serializer(parceiros, many=True)
         return Response(serialazer.data, status=status.HTTP_200_OK)
 
@@ -801,16 +788,9 @@ class CartoesAPIv1(ModelViewSet):
         }
         return Response(data, status=status.HTTP_200_OK)
 
-
-<<<<<<< HEAD
-class FluxoCaixaAPIv1(ModelViewSet):
-    queryset = models.Diario.objects.all()
-    serializer_class = serialazers.DiarioSerialazers
-=======
 class FluxoCaixaViewSet(ModelViewSet):
     queryset = models.Diario.objects.all()
     serializer_class = serialazers.DiarioSerializer
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
     pagination_class = DefaultPagination
     permission_classes = [IsAuthenticated]
     
@@ -897,14 +877,8 @@ class FluxoCaixaViewSet(ModelViewSet):
                 new_item = {
                     'id': i.id,
                     'banco': str(i.banco.nomebanco) if i.banco else None,
-<<<<<<< HEAD
                     'parceiro': str(i.parceiro) if not nomecartao else nomecartao,
                     'categoria': str(i.categoria) if i.descricao != '<CRED.CARD>' else 'Cartão de Crédito',
-=======
-                    'parceiro': i.parceiro if not nomecartao else nomecartao,
-                    'categoria': (i.categoria if i.descricao != '<CRED.CARD>'
-                                else 'Cartão de Crédito'),
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
                     'valor_entra': str(valor_entra) if valor_entra else '',
                     'valor_sai': str(valor_sai) if valor_sai else '',
                     'valor_saldo': str(saldo_atual),
@@ -1042,15 +1016,9 @@ class FluxoCaixaViewSet(ModelViewSet):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-<<<<<<< HEAD
-class ResumoDiarioAPIv1(ModelViewSet):
-    queryset = models.Diario.objects.none()
-    serializer_class = serialazers.DiarioSerialazers
-=======
 class ResumoDiarioViewSet(ModelViewSet):
     queryset = models.Diario.objects.none()
     serializer_class = serialazers.DiarioSerializer
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
     pagination_class = DefaultPagination
     permission_classes = [IsAuthenticated]
 
@@ -1142,15 +1110,9 @@ class ResumoDiarioViewSet(ModelViewSet):
         }
         return Response(retorno, status=status.HTTP_200_OK)
 
-<<<<<<< HEAD
-class ResumoCategoriaAPIv1(ModelViewSet):
-    queryset = models.Diario.objects.none()
-    serializer_class = serialazers.DiarioSerialazers
-=======
 class ResumoCategoriaViewSet(ModelViewSet):
     queryset = models.Diario.objects.none()
     serializer_class = serialazers.DiarioSerializer
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
     pagination_class = DefaultPagination
     permission_classes = [IsAuthenticated]
 
@@ -1257,15 +1219,9 @@ class ResumoCategoriaViewSet(ModelViewSet):
         }
         return Response(response_data, status=status.HTTP_200_OK)
 
-<<<<<<< HEAD
-class ResumoParceiroAPIv1(ModelViewSet):
-    queryset = models.Diario.objects.none()
-    serializer_class = serialazers.DiarioSerialazers
-=======
 class ResumoParceiroViewSet(ModelViewSet):
     queryset = models.Diario.objects.none()
     serializer_class = serialazers.DiarioSerializer
->>>>>>> 65ab2d5 (Implementação da estrutura inicial da API financeira com modelos, rotas e configuração de autenticação JWT.)
     pagination_class = DefaultPagination
     permission_classes = [IsAuthenticated]
 

628e8051a91c1589131ae29f54b7a5f079572802
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2025-04-27 19:12:18 -0300

    Registrar o modelo Diario no admin com configuração de exibição, filtro e busca

--- financial/manager/admin.py
+++ financial/manager/admin.py
@@ -6,5 +6,10 @@ admin.site.register(consistente.ConsistenteUsuario)
 admin.site.register(consistente.Banco)
 admin.site.register(consistente.Categoria)
 admin.site.register(consistente.Parceiro)
-admin.site.register(consistente.Diario)
 admin.site.register(consistente.ConsistenteUserCuston)
+
+@admin.register(consistente.Diario)
+class DiarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao' ,'tipomov', 'create_user')
+    list_filter = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao' ,'tipomov')
+    search_fields = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao' ,'tipomov')

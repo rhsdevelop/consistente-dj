4582f4f360a980dcfab730613242406546bfb932
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2025-04-28 09:20:53 -0300

    Corrigir formatação do código no registro do modelo Diario no admin

--- financial/manager/admin.py
+++ financial/manager/admin.py
@@ -10,6 +10,6 @@ admin.site.register(consistente.ConsistenteUserCuston)
 
 @admin.register(consistente.Diario)
 class DiarioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao' ,'tipomov', 'create_user')
-    list_filter = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao' ,'tipomov')
-    search_fields = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao' ,'tipomov')
+    list_display = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao', 'tipomov', 'create_user')
+    list_filter = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao', 'tipomov')
+    search_fields = ('id', 'consistente_cliente', 'parceiro', 'banco', 'descricao', 'tipomov')
